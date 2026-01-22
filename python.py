eaef03656d63323420508207fcd0df8f8d0377bd
Praneetha <praneethagulipilli2470@gmail.com>
2026-01-22 17:04:30 +0530

    Update python.py

--- python.py
+++ python.py
@@ -2,3 +2,7 @@ num1 = input('Enter first number: ')
 num2 = input('Enter second number: ')
 sum = float(num1) + float(num2)
 print(f'The sum of {num1} and {num2} is {sum}')
+
+new line added
+by user
+dev 1
